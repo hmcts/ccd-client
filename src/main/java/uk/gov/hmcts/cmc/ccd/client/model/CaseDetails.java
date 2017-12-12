b9413a86d8acdfe995be997a7c31046dfc78192d
sabahirfan <sabah.irfan@gmail.com>
2017-12-12 15:37:44 +0000

    ROC-2760: Add core case data store client (#3)

---
+++ src/main/java/uk/gov/hmcts/cmc/ccd/client/model/CaseDetails.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.cmc.ccd.client.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CaseDetails {
+    private Long id;
+    private String jurisdiction;
+    @JsonProperty("case_type_id")
+    private String caseTypeId;
+    @JsonProperty("created_date")
+    private LocalDateTime createdDate;
+    @JsonProperty("last_modified")
+    private LocalDateTime lastModified;
+    private String state;
+    @JsonProperty("locked_by_user_id")
+    private Integer lockedBy;
+    @JsonProperty("security_level")
+    private Integer securityLevel;
+    @JsonProperty("case_data")
+    private Map<String, Object> data;
+    @JsonProperty("security_classification")
+    private Classification securityClassification;
+}
