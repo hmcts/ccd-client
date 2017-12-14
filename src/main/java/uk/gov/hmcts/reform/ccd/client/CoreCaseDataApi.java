bece26eb11b80b43828ba7cf7e4db8c021c2e647
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-12-14 18:55:19 +0000

    remove response entity from clients

--- src/main/java/uk/gov/hmcts/reform/ccd/client/CoreCaseDataApi.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/CoreCaseDataApi.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -33,25 +32,25 @@ public interface CoreCaseDataApi {
             + "{eventId}/token",
         headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
     )
-    ResponseEntity<StartEventResponse> start(@RequestHeader(AUTHORIZATION) String authorisation,
-                                             @RequestHeader("ServiceAuthorization") String serviceAuthorization,
-                                             @PathVariable String userId,
-                                             @PathVariable String jurisdictionId,
-                                             @PathVariable String caseType,
-                                             @PathVariable String eventId
+    StartEventResponse start(@RequestHeader(AUTHORIZATION) String authorisation,
+                             @RequestHeader("ServiceAuthorization") String serviceAuthorization,
+                             @PathVariable String userId,
+                             @PathVariable String jurisdictionId,
+                             @PathVariable String caseType,
+                             @PathVariable String eventId
     );
 
     @RequestMapping(
         method = RequestMethod.POST,
         value = "/caseworkers/{userId}/jurisdictions/{jurisdictionId}/case-types/{caseType}/cases"
     )
-    ResponseEntity<CaseDetails> submit(@RequestHeader(AUTHORIZATION) String authorisation,
-                                       @RequestHeader("ServiceAuthorization") String serviceAuthorisation,
-                                       @PathVariable String userId,
-                                       @PathVariable String jurisdictionId,
-                                       @PathVariable String caseType,
-                                       @RequestParam("ignore-warning") boolean ignoreWarning,
-                                       @RequestBody CaseDataContent caseDataContent
+    CaseDetails submit(@RequestHeader(AUTHORIZATION) String authorisation,
+                       @RequestHeader("ServiceAuthorization") String serviceAuthorisation,
+                       @PathVariable String userId,
+                       @PathVariable String jurisdictionId,
+                       @PathVariable String caseType,
+                       @RequestParam("ignore-warning") boolean ignoreWarning,
+                       @RequestBody CaseDataContent caseDataContent
     );
 
     @RequestMapping(
