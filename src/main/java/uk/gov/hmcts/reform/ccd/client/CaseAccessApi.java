99cfda7a038fc70aa647b749a5dcfc93300cc2c8
Tim Jacomb <tim.jacomb@hmcts.net>
2018-01-22 17:07:03 +0000

    Add authorisation headers

--- src/main/java/uk/gov/hmcts/reform/ccd/client/CaseAccessApi.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/CaseAccessApi.java
@@ -4,12 +4,15 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import uk.gov.hmcts.reform.ccd.client.model.UserId;
 
 import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @FeignClient(name = "ccd-access-api", url = "${core_case_data.api.url}",
         configuration = CoreCaseDataConfiguration.class)
@@ -21,6 +24,8 @@ public interface CaseAccessApi {
             consumes = MediaType.ALL_VALUE
     )
     List<String> findCaseIdsGivenUserIdHasAccessTo(
+            @RequestHeader(AUTHORIZATION) String authorisation,
+            @RequestHeader("ServiceAuthorization") String serviceAuthorization,
             @PathVariable("uid") final Integer uid,
             @PathVariable("jid") final String jurisdictionId,
             @PathVariable("ctid") final String caseTypeId,
@@ -33,6 +38,8 @@ public interface CaseAccessApi {
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
     void grantAccessToCase(
+            @RequestHeader(AUTHORIZATION) String authorisation,
+            @RequestHeader("ServiceAuthorization") String serviceAuthorization,
             @PathVariable("uid") final String uid,
             @PathVariable("jid") final String jurisdictionId,
             @PathVariable("ctid") final String caseTypeId,
@@ -46,6 +53,8 @@ public interface CaseAccessApi {
             consumes = MediaType.ALL_VALUE
     )
     void revokeAccessToCase(
+            @RequestHeader(AUTHORIZATION) String authorisation,
+            @RequestHeader("ServiceAuthorization") String serviceAuthorization,
             @PathVariable("uid") final Integer uid,
             @PathVariable("jid") final String jurisdictionId,
             @PathVariable("ctid") final String caseTypeId,
