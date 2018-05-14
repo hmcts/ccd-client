e4ccdeaa88ff8f8623dfae19e4a0a8672434387f
Tim Jacomb <t.jacomb@kainos.com>
2018-05-14 09:25:40 +0100

    Make API consistent and allow unknown properties (#28)
    Allow unknown properties to allow for api extension

--- src/main/java/uk/gov/hmcts/reform/ccd/client/model/PaginatedSearchMetadata.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/model/PaginatedSearchMetadata.java
@@ -1,7 +1,11 @@
 package uk.gov.hmcts.reform.ccd.client.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PaginatedSearchMetadata {
 
     @JsonProperty("total_results_count")
