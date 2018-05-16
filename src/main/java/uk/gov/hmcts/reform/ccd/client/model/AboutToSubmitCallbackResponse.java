c8b031e344f7b9caefb9112da4e19c03bf946c3f
Tim Jacomb <tim.jacomb@hmcts.net>
2018-05-16 16:15:27 +0100

    ROC-3728 Callback support

---
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/model/AboutToSubmitCallbackResponse.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.ccd.client.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+public class AboutToSubmitCallbackResponse {
+    private Map<String, Object> data;
+    @JsonProperty("data_classification")
+    private Map<String, Object> dataClassification;
+    @JsonProperty("security_classification")
+    private Map<String, Object> securityClassification;
+    private List<String> errors;
+    private List<String> warnings;
+
+}
---
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/model/CaseCallback.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.ccd.client.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@Builder
+public class CaseCallback {
+    @JsonProperty("case_details")
+    private Map<String, Object> caseDetails;
+    @JsonProperty("case_details_before")
+    private Map<String, Object> caseDetailsBefore;
+    @JsonProperty("event_id")
+    private String eventId;
+}
