9692e06e9c6db91b78fe3cf4b79698fe0328c5b7
Rafal Kondratowicz <rkondr@gmail.com>
2018-09-25 15:12:09 +0100

    Add constants for ccd callback types

---
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/model/CallbackTypes.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.ccd.client.model;
+
+public final class CallbackTypes {
+    public static final String ABOUT_TO_START = "about-to-start";
+    public static final String ABOUT_TO_SUBMIT = "about-to-submit";
+    public static final String SUBMITTED = "submitted";
+
+    private CallbackTypes() {
+        // util class
+    }
+}
