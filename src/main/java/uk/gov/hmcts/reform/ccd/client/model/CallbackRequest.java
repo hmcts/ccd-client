0fbbd51bef68b11af019aee74c0a6ddf8a39a78c
Tim Jacomb <t.jacomb@kainos.com>
2018-10-19 11:42:59 +0200

    Use correct object for CallbackRequest (#37)

--- src/main/java/uk/gov/hmcts/reform/ccd/client/model/CallbackRequest.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/model/CallbackRequest.java
@@ -4,17 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.Map;
-
 @Data
 @Builder
 public class CallbackRequest {
 
     @JsonProperty("case_details")
-    private Map<String, Object> caseDetails;
+    private CaseDetails caseDetails;
 
     @JsonProperty("case_details_before")
-    private Map<String, Object> caseDetailsBefore;
+    private CaseDetails caseDetailsBefore;
 
     @JsonProperty("event_id")
     private String eventId;
