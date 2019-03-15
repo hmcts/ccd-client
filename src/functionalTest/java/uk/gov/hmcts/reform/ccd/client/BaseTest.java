f7ff28938c8496549cb292155f4ec5031bb9928d
Tim Jacomb <t.jacomb@kainos.com>
2019-03-15 10:33:32 +0000

    RPE-1005 Add CaseAccessApi functional test (#48)
    Add coverage to CaseAccessApi

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/ccd/client/BaseTest.java
@@ -0,0 +1,155 @@
+package uk.gov.hmcts.reform.ccd.client;
+
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.IdamTestApi;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+import uk.gov.hmcts.reform.idam.client.models.test.CreateUserRequest;
+import uk.gov.hmcts.reform.idam.client.models.test.UserGroup;
+import uk.gov.hmcts.reform.idam.client.models.test.UserRole;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.singletonList;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {TestConfiguration.class})
+public class BaseTest {
+
+    // can be found here: https://github.com/hmcts/ccd-definition-store-api/blob/master/aat/src/resource/CCD_CNP_27.xlsx
+    static final String JURISDICTION = "AUTOTEST1";
+    static final String CASE_TYPE = "AAT";
+    static final String UPDATE_CASE_EVENT = "START_PROGRESS";
+    private static final String CREATE_CASE_EVENT = "CREATE";
+
+    private static final Map<String, String> CREATE_CASE_DATA = new HashMap<String, String>() {
+        {
+            put("TextField", "text...");
+        }
+    };
+
+    @Autowired
+    protected IdamClient idamClient;
+
+    @Autowired
+    protected IdamTestApi idamTestApi;
+
+    @Autowired
+    protected CoreCaseDataApi coreCaseDataApi;
+
+    @Autowired
+    protected AuthTokenGenerator authTokenGenerator;
+
+    User createCitizen() {
+        String citizenEmail = "autocitizen-ccdclient@hmcts.net";
+        try {
+            idamTestApi.createUser(CreateUserRequest.builder()
+                    .email(citizenEmail)
+                    .userGroup(new UserGroup("citizens"))
+                    .build());
+        } catch (FeignException e) {
+            if (e.status() == HttpStatus.BAD_REQUEST.value()) {
+                log.info("Bad request from idam, probably user already exists, continuing");
+            } else {
+                throw e;
+            }
+        }
+
+        String token = idamClient.authenticateUser(citizenEmail, CreateUserRequest.DEFAULT_PASSWORD);
+        UserDetails userDetails = idamClient.getUserDetails(token);
+
+        return new User(token, userDetails);
+    }
+
+    User createCaseworker() {
+        return createCaseworker("autocaseworker-ccdclient@hmcts.net");
+    }
+
+    User createCaseworker(String email) {
+        try {
+            idamTestApi.createUser(CreateUserRequest.builder()
+                    .email(email)
+                    .userGroup(new UserGroup("caseworker"))
+                    .roles(singletonList(new UserRole("caseworker-autotest1")))
+                    .build());
+        } catch (FeignException e) {
+            if (e.status() == HttpStatus.BAD_REQUEST.value()) {
+                log.info("Bad request from idam, probably user already exists, continuing");
+            } else {
+                throw e;
+            }
+        }
+
+        String token = idamClient.authenticateUser(email, CreateUserRequest.DEFAULT_PASSWORD);
+        UserDetails userDetails = idamClient.getUserDetails(token);
+
+        return new User(token, userDetails);
+    }
+
+    CaseDetails createCaseForCaseworker(User caseworker) {
+        StartEventResponse startEventResponse = coreCaseDataApi.startForCaseworker(
+                caseworker.getAuthToken(),
+                authTokenGenerator.generate(),
+                caseworker.getUserDetails().getId(),
+                JURISDICTION,
+                CASE_TYPE,
+                CREATE_CASE_EVENT
+        );
+
+        CaseDataContent caseDataContent = CaseDataContent.builder()
+                .eventToken(startEventResponse.getToken())
+                .event(Event.builder().id(startEventResponse.getEventId()).build())
+                .data(CREATE_CASE_DATA)
+                .build();
+
+        return coreCaseDataApi.submitForCaseworker(
+                caseworker.getAuthToken(),
+                authTokenGenerator.generate(),
+                caseworker.getUserDetails().getId(),
+                JURISDICTION,
+                CASE_TYPE,
+                true,
+                caseDataContent
+        );
+    }
+
+    CaseDetails createCaseForCitizen(User citizen) {
+        StartEventResponse startEventResponse = coreCaseDataApi.startForCitizen(
+                citizen.getAuthToken(),
+                authTokenGenerator.generate(),
+                citizen.getUserDetails().getId(),
+                JURISDICTION,
+                CASE_TYPE,
+                CREATE_CASE_EVENT
+        );
+
+        CaseDataContent caseDataContent = CaseDataContent.builder()
+                .eventToken(startEventResponse.getToken())
+                .event(Event.builder().id(startEventResponse.getEventId()).build())
+                .data(CREATE_CASE_DATA)
+                .build();
+
+        return coreCaseDataApi.submitForCitizen(
+                citizen.getAuthToken(),
+                authTokenGenerator.generate(),
+                citizen.getUserDetails().getId(),
+                JURISDICTION,
+                CASE_TYPE,
+                true,
+                caseDataContent
+        );
+    }
+}
--- src/functionalTest/java/uk/gov/hmcts/reform/ccd/client/CoreCaseDataApiTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/ccd/client/CoreCaseDataApiTest.java
@@ -1,31 +1,26 @@
 package uk.gov.hmcts.reform.ccd.client;
 
-import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
-import uk.gov.hmcts.reform.idam.client.IdamClient;
-import uk.gov.hmcts.reform.idam.client.IdamTestApi;
-import uk.gov.hmcts.reform.idam.client.models.test.CreateUserRequest;
-import uk.gov.hmcts.reform.idam.client.models.test.UserGroup;
-import uk.gov.hmcts.reform.idam.client.models.test.UserRole;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -33,84 +28,236 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("Core case data api")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {TestConfiguration.class})
-public class CoreCaseDataApiTest {
+// IntelliJ doesn't seem to pickup any of the clients here
+class CoreCaseDataApiTest extends BaseTest {
 
-    // can be found here: https://github.com/hmcts/ccd-definition-store-api/blob/master/aat/src/resource/CCD_CNP_27.xlsx
-    private static final String JURISDICTION = "AUTOTEST1";
-    private static final String CASE_TYPE = "AAT";
-    private static final String CREATE_CASE_EVENT = "CREATE";
+    private static final Map<String, String> UPDATE_CASE_DATA = new HashMap<String, String>() {
+        {
+            put("TextField", "text updated");
+        }
+    };
+
+    private static final Map<String, String> SEARCH_CRITERIA = new HashMap<String, String>() {
+        {
+            put("case.TextField", "text...");
+        }
+    };
 
     @Autowired
     private CoreCaseDataApi coreCaseDataApi;
 
     @Autowired
-    private IdamClient idamClient;
-
-    @Autowired
-    private IdamTestApi idamTestApi;
-
-    @Autowired
     private AuthTokenGenerator authTokenGenerator;
 
-    private static String idamCaseWorkerToken;
-    private static String idamCaseWorkerId;
+    private static User caseWorker;
+    private static User citizen;
 
-    @BeforeEach
-    void init() {
-        String caseworkerEmail = "autocaseworker-test@hmcts.net";
-        try {
-            idamTestApi.createUser(CreateUserRequest.builder()
-                    .email(caseworkerEmail)
-                    .userGroup(new UserGroup("caseworker"))
-                    .roles(singletonList(new UserRole("caseworker-autotest1")))
-                    .build());
-        } catch (FeignException e) {
-            if (e.status() == HttpStatus.BAD_REQUEST.value()) {
-                log.info("Bad request from idam, probably user already exists, continuing");
-            } else {
-                throw e;
+    @Nested
+    @DisplayName("Case worker")
+    class CaseWorker {
+        @BeforeEach
+        void init() {
+            caseWorker = createCaseworker();
+        }
+
+        @Test
+        @DisplayName("Should be able to retrieve a case")
+        void getCaseForCaseworker() {
+            CaseDetails caseDetails = createCaseForCaseworker(caseWorker);
+
+            CaseDetails theCase = coreCaseDataApi
+                    .getCase(caseWorker.getAuthToken(), authTokenGenerator.generate(), caseDetails.getId() + "");
+
+            assertThat(theCase.getId())
+                    .isEqualTo(caseDetails.getId());
+        }
+    }
+
+    @Nested
+    class V1 {
+
+        @Nested
+        @DisplayName("Case worker")
+        class CaseWorker {
+
+            @BeforeEach
+            void init() {
+                caseWorker = createCaseworker();
+            }
+
+            @Test
+            @DisplayName("Should be able to create a case")
+            void createCase() {
+                CaseDetails caseDetails = createCaseForCaseworker(caseWorker);
+
+                assertThat(caseDetails.getData().get("TextField"))
+                        .isEqualTo("text...");
+            }
+
+            @Test
+            @DisplayName("Should be able to retrieve a case")
+            void getCase() {
+                CaseDetails caseDetails = createCaseForCaseworker(caseWorker);
+
+                CaseDetails theCase = coreCaseDataApi
+                        .readForCaseWorker(
+                                caseWorker.getAuthToken(),
+                                authTokenGenerator.generate(),
+                                caseWorker.getUserDetails().getId(),
+                                JURISDICTION,
+                                CASE_TYPE,
+                                caseDetails.getId() + ""
+                        );
+
+                assertThat(theCase.getId())
+                        .isEqualTo(caseDetails.getId());
+            }
+
+            @Test
+            @DisplayName("Should be able to search to retrieve a case")
+            void search() {
+                createCaseForCaseworker(caseWorker);
+
+                List<CaseDetails> caseDetails1 = coreCaseDataApi.searchForCaseworker(
+                        caseWorker.getAuthToken(),
+                        authTokenGenerator.generate(),
+                        caseWorker.getUserDetails().getId(),
+                        JURISDICTION,
+                        CASE_TYPE,
+                        SEARCH_CRITERIA
+                );
+
+                assertThat(caseDetails1.size()).isGreaterThan(0);
+                assertThat(caseDetails1.get(0).getData().get("TextField")).isEqualTo("text...");
+            }
+
+            @Test
+            @DisplayName("Should be able to update a case")
+            void update() {
+                CaseDetails caseDetails = createCaseForCaseworker(caseWorker);
+
+                StartEventResponse startEventResponse = coreCaseDataApi.startEventForCaseWorker(
+                        caseWorker.getAuthToken(),
+                        authTokenGenerator.generate(),
+                        caseWorker.getUserDetails().getId(),
+                        JURISDICTION,
+                        CASE_TYPE,
+                        caseDetails.getId() + "",
+                        UPDATE_CASE_EVENT
+                );
+
+                CaseDataContent caseDataContent = CaseDataContent.builder()
+                        .eventToken(startEventResponse.getToken())
+                        .event(Event.builder().id(startEventResponse.getEventId()).build())
+                        .data(UPDATE_CASE_DATA)
+                        .build();
+
+                CaseDetails updatedCase = coreCaseDataApi.submitEventForCaseWorker(
+                        caseWorker.getAuthToken(),
+                        authTokenGenerator.generate(),
+                        caseWorker.getUserDetails().getId(),
+                        JURISDICTION,
+                        CASE_TYPE,
+                        caseDetails.getId() + "",
+                        true,
+                        caseDataContent
+                );
+
+                assertThat(updatedCase.getData().get("TextField")).isEqualTo("text updated");
             }
         }
 
-        idamCaseWorkerToken = idamClient.authenticateUser(caseworkerEmail, CreateUserRequest.DEFAULT_PASSWORD);
-        idamCaseWorkerId = idamClient.getUserDetails(idamCaseWorkerToken).getId();
-    }
+        @Nested
+        @DisplayName("Citizen")
+        @Disabled("Auto test jurisdiction doesn't have citizen events currently, it's on the CCD backlog to do soon")
+        class Citizen {
 
-    @Test
-    @DisplayName("Should be able to create a case for a caseworker")
-    void createCaseForCaseworker() {
-        StartEventResponse startEventResponse = coreCaseDataApi.startForCaseworker(
-                idamCaseWorkerToken,
-                authTokenGenerator.generate(),
-                idamCaseWorkerId,
-                JURISDICTION,
-                CASE_TYPE,
-                CREATE_CASE_EVENT
-        );
-
-        Map<String, String> caseData = new HashMap<String, String>() {
-            {
-                put("TextField", "text...");
+            @BeforeEach
+            void init() {
+                citizen = createCitizen();
             }
-        };
 
-        CaseDataContent caseDataContent = CaseDataContent.builder()
-                .eventToken(startEventResponse.getToken())
-                .event(Event.builder().id(startEventResponse.getEventId()).build())
-                .data(caseData)
-                .build();
+            @Test
+            @DisplayName("Should be able to create a case")
+            void createCase() {
+                CaseDetails caseDetails = createCaseForCitizen(citizen);
 
-        CaseDetails caseDetails = coreCaseDataApi.submitForCaseworker(
-                idamCaseWorkerToken,
-                authTokenGenerator.generate(),
-                idamCaseWorkerId,
-                JURISDICTION,
-                CASE_TYPE,
-                true,
-                caseDataContent
-        );
+                assertThat(caseDetails.getData().get("TextField"))
+                        .isEqualTo("text...");
+            }
 
-        assertThat(caseDetails.getData().get("TextField"))
-                .isEqualTo("text...");
+            @Test
+            @DisplayName("Should be able to retrieve a case")
+            void getCase() {
+                CaseDetails caseDetails = createCaseForCitizen(citizen);
+
+                CaseDetails theCase = coreCaseDataApi
+                        .readForCitizen(
+                                citizen.getAuthToken(),
+                                authTokenGenerator.generate(),
+                                citizen.getUserDetails().getId(),
+                                JURISDICTION,
+                                CASE_TYPE,
+                                caseDetails.getId() + ""
+                        );
+
+                assertThat(theCase.getId())
+                        .isEqualTo(caseDetails.getId());
+            }
+
+            @Test
+            @DisplayName("Should be able to search to retrieve a case")
+            void search() {
+                createCaseForCitizen(citizen);
+
+                List<CaseDetails> caseDetails1 = coreCaseDataApi.searchForCitizen(
+                        citizen.getAuthToken(),
+                        authTokenGenerator.generate(),
+                        citizen.getUserDetails().getId(),
+                        JURISDICTION,
+                        CASE_TYPE,
+                        SEARCH_CRITERIA
+                );
+
+                assertThat(caseDetails1.size()).isGreaterThan(0);
+                assertThat(caseDetails1.get(0).getData().get("TextField")).isEqualTo("text...");
+            }
+
+            @Test
+            @DisplayName("Should be able to update a case")
+            void update() {
+                CaseDetails caseDetails = createCaseForCitizen(citizen);
+
+                StartEventResponse startEventResponse = coreCaseDataApi.startEventForCitizen(
+                        citizen.getAuthToken(),
+                        authTokenGenerator.generate(),
+                        citizen.getUserDetails().getId(),
+                        JURISDICTION,
+                        CASE_TYPE,
+                        caseDetails.getId() + "",
+                        UPDATE_CASE_EVENT
+                );
+
+                CaseDataContent caseDataContent = CaseDataContent.builder()
+                        .eventToken(startEventResponse.getToken())
+                        .event(Event.builder().id(startEventResponse.getEventId()).build())
+                        .data(UPDATE_CASE_DATA)
+                        .build();
+
+                CaseDetails updatedCase = coreCaseDataApi.submitEventForCitizen(
+                        caseWorker.getAuthToken(),
+                        authTokenGenerator.generate(),
+                        caseWorker.getUserDetails().getId(),
+                        JURISDICTION,
+                        CASE_TYPE,
+                        caseDetails.getId() + "",
+                        true,
+                        caseDataContent
+                );
+
+                assertThat(updatedCase.getData().get("TextField")).isEqualTo("text updated");
+            }
+        }
+
     }
 }
