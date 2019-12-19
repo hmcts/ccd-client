68100c8a21e20086fe7501416513f2887e0c4a23
Dave Oxtoby <david.oxtoby@hmcts.net>
2019-12-19 10:35:24 +0000

    Review fixes

--- src/main/java/uk/gov/hmcts/reform/ccd/client/CaseEventsApi.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/CaseEventsApi.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.ccd.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +22,7 @@ import static uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi.SERVICE_AUTHORIZATI
 )
 public interface CaseEventsApi {
 
-    @RequestMapping(
-        method = RequestMethod.GET,
+    @GetMapping(
         value = "/caseworkers/{userId}/jurisdictions/{jurisdictionId}/case-types/{caseType}/cases/{caseId}/events",
         headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
     )
