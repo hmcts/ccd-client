2467debfd2b2274ba1cd69fc0d9c9111f733cbb0
Dave Oxtoby <david.oxtoby@hmcts.net>
2019-12-19 10:37:09 +0000

    Remove imports

--- src/main/java/uk/gov/hmcts/reform/ccd/client/CaseEventsApi.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/CaseEventsApi.java
@@ -4,8 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import uk.gov.hmcts.reform.ccd.client.model.CaseEventDetails;
 
 import java.util.List;
