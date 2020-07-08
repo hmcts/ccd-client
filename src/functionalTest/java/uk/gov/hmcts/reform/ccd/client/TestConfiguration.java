64fac45a15ce360c4ec8146ad997aae6f126d3f8
dharmendra kumar <dharmendra.kumar@hmcts.net>
2020-07-08 22:26:26 +0100

    Enable idam feign client in test configuration (#61)

--- src/functionalTest/java/uk/gov/hmcts/reform/ccd/client/TestConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/ccd/client/TestConfiguration.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.ccd.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
@@ -8,13 +9,14 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
+@EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.idam.client"})
 class TestConfiguration {
 
     @Bean
     public AuthTokenGenerator authTokenGenerator(
-            @Value("${idam.s2s-auth.totp_secret}") final String secret,
-            @Value("${idam.s2s-auth.microservice}") final String microService,
-            final ServiceAuthorisationApi serviceAuthorisationApi
+        @Value("${idam.s2s-auth.totp_secret}") final String secret,
+        @Value("${idam.s2s-auth.microservice}") final String microService,
+        final ServiceAuthorisationApi serviceAuthorisationApi
     ) {
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
     }
