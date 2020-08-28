1a1ddc3db7fa9d4beca29ec056b110341a7c5a96
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-08-28 12:39:35 +0100

    RDM-9370: changed ObjectMapper to Singleton scope

--- src/main/java/uk/gov/hmcts/reform/ccd/client/CoreCaseDataConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/CoreCaseDataConfiguration.java
@@ -3,19 +3,22 @@ package uk.gov.hmcts.reform.ccd.client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.codec.Decoder;
 import feign.jackson.JacksonDecoder;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import uk.gov.hmcts.reform.ccd.client.model.SearchCriteria;
 
 class CoreCaseDataConfiguration {
 
-    @Bean
+    @Bean(name = "defaultObjectMapper")
     @Primary
-    public ObjectMapper objectMapper() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
         builder.failOnUnknownProperties(false);
-        return builder.build();
+        ObjectMapper objectMapper = builder.build();
+        return objectMapper;
     }
 
     @Bean
