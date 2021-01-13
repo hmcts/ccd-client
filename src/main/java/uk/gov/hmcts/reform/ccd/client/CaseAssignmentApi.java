3a06da2f02e1dc11f8ef4e080f515adfef3795b4
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2021-01-13 16:14:44 +0000

    checkstyle fixes

--- src/main/java/uk/gov/hmcts/reform/ccd/client/CaseAssignmentApi.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/CaseAssignmentApi.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import uk.gov.hmcts.reform.ccd.client.model.CaseAssignmentUserRolesResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CaseAssignmentUserRolesRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseAssignmentUserRolesResource;
+import uk.gov.hmcts.reform.ccd.client.model.CaseAssignmentUserRolesResponse;
 
 import java.util.List;
 
