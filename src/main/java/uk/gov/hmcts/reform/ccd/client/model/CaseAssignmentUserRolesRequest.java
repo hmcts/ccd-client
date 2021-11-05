30df36f153d39093ed9e27edabdfda6c20cdb50d
Linus Norton <linusnorton@gmail.com>
2021-11-05 12:35:58 +0000

    More jackson

--- src/main/java/uk/gov/hmcts/reform/ccd/client/model/CaseAssignmentUserRolesRequest.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/client/model/CaseAssignmentUserRolesRequest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Getter
 @Data
 @Builder
+@Jacksonized
 public class CaseAssignmentUserRolesRequest {
 
     @JsonProperty("case_users")
